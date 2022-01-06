d3b00286eabe8a324f719352ed7c94a15806d6dc
Nathan Jaremko <njaremko@wealthsimple.com>
2022-01-06 11:17:38 -0500

    Add homepage

--- src/Tickerizer.hs
+++ src/Tickerizer.hs
@@ -20,6 +20,8 @@ import Network.Wai.Handler.Warp (run)
 import Data.Trie (Trie )
 import qualified Data.Trie as TE
 import Data.Text.Encoding as TTE
+import Servant.HTML.Lucid
+import Lucid (Html, body_, p_, a_, With (with), href_)
 
 newtype Port = Port Int
   deriving newtype (Envy.Var)
@@ -70,6 +72,16 @@ type HealthApi = "health" :> "alive" :> Get '[JSON] Text
 
 type TickerizeApi = "tickerize" :> Capture "input" Text :> Get '[JSON] Text
 
+type RootEndpoint = Get '[HTML] (Html ())
+
+rootEndpoint :: Web sig m => ServerT RootEndpoint m
+rootEndpoint = asdf
+  where
+    asdf :: Web sig m => m (Html ())
+    asdf = do
+      AppState{baseUrl = t} <- ask @AppState
+      return $ body_ $ p_ "To use this site, navigate to " <> with a_ [href_  (coerce t <> "/tickerize/allegedly%20brainstorming")] "Here"
+
 healthApi :: Web sig m => ServerT HealthApi m
 healthApi = return "Ok"
 
@@ -103,15 +115,13 @@ processInput t input =
           (_, leftover) = T.splitAt (T.length looked) remaining
           in (firstLetter <> looked, leftover)
 
-type Api = HealthApi :<|> TickerizeApi
+type Api = HealthApi :<|> TickerizeApi :<|> RootEndpoint
 
 apiProxy :: Proxy Api
 apiProxy = Proxy
 
 server :: ServerT Api WebConcrete
-server = api
-  where
-    api = healthApi :<|> tickerizeApi
+server = healthApi :<|> tickerizeApi :<|> rootEndpoint
 
 runServer :: AppState -> IO ()
 runServer appState@AppState {port = Port port} = do
