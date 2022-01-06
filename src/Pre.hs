846ba0fb5b8ebdd54d04808a44642f7300c44988
Nathan Jaremko <njaremko@wealthsimple.com>
2022-01-06 12:51:17 -0500

    Cleanup

--- src/Pre.hs
+++ src/Pre.hs
@@ -11,7 +11,11 @@ module Pre
     ReaderC,
     Has,
     Lift,
-    LiftC
+    LiftC,
+    Port,
+    BaseUrl,
+    openPort,
+    openBaseUrl
   )
 where
 
@@ -19,3 +23,33 @@ import Control.Carrier.Lift (Has, Lift, LiftC, runM, sendIO, sendM)
 import Control.Carrier.Reader (Reader, ReaderC, ask, runReader)
 import Control.Exception (throwIO, try)
 import Relude hiding (Reader, ask, id, runReader)
+import System.Envy (FromEnv, ToEnv)
+import qualified System.Envy as Envy
+
+newtype Port = Port Int
+  deriving newtype (Envy.Var, Show)
+
+openPort :: Port -> Int
+openPort (Port port) = port
+
+instance FromEnv Port where
+  fromEnv _ = Port <$> Envy.env "PORT"
+
+instance ToEnv Port where
+  toEnv port = Envy.makeEnv [ "PORT" Envy..= port ]
+
+newtype BaseUrl = BaseUrl Text
+  deriving newtype (Show, Semigroup, Monoid)
+
+openBaseUrl :: BaseUrl -> Text
+openBaseUrl (BaseUrl baseUrl) = baseUrl
+
+instance FromEnv BaseUrl where
+  fromEnv _ = BaseUrl 
+    <$> Envy.env "BASE_URL" 
+
+instance ToEnv BaseUrl where
+  toEnv (BaseUrl baseUrl) =
+    Envy.makeEnv
+      [ "BASE_URL" Envy..= baseUrl
+      ]
--- src/Tickerizer.hs
+++ src/Tickerizer.hs
@@ -13,8 +13,6 @@ module Tickerizer
 
 import Pre
 import Servant
-import System.Envy (FromEnv, ToEnv)
-import qualified System.Envy as Envy
 import qualified Data.Text as T
 import Network.Wai.Handler.Warp (run)
 import Data.Trie (Trie )
@@ -22,28 +20,6 @@ import qualified Data.Trie as TE
 import Data.Text.Encoding as TTE
 import Servant.HTML.Lucid
 import Lucid (Html, body_, p_, a_, With (with), href_)
-
-newtype Port = Port Int
-  deriving newtype (Envy.Var)
-
-instance FromEnv Port where
-  fromEnv _ = Port <$> Envy.env "PORT"
-
-instance ToEnv Port where
-  toEnv port = Envy.makeEnv [ "PORT" Envy..= port ]
-
-newtype BaseUrl = BaseUrl {baseUrl :: Text}
-  deriving newtype (Show, Semigroup, Monoid)
-
-instance FromEnv BaseUrl where
-  fromEnv _ = BaseUrl 
-    <$> Envy.env "BASE_URL" 
-
-instance ToEnv BaseUrl where
-  toEnv BaseUrl {baseUrl} =
-    Envy.makeEnv
-      [ "BASE_URL" Envy..= baseUrl
-      ]
 
 type Web sig m =
   ( Has (Reader AppState) sig m,
@@ -60,6 +36,62 @@ data AppState = AppState
     tickerTrie :: Trie ()
   }
 
+type HealthApi = "health" :> "alive" :> Get '[JSON] Text
+
+type TickerizeApi = "tickerize" :> Capture "input" Text :> Get '[JSON] Text
+
+type RootEndpoint = Get '[HTML] (Html ())
+
+type Api = HealthApi :<|> TickerizeApi :<|> RootEndpoint
+
+rootEndpoint :: Web sig m => ServerT RootEndpoint m
+rootEndpoint = renderHome
+  where
+    renderHome :: Web sig m => m (Html ())
+    renderHome = do
+      AppState{baseUrl} <- ask @AppState
+      return . body_ $ do
+        p_ "To use this site, navigate to " <> with a_ [href_  (openBaseUrl baseUrl <> "/tickerize/allegedly%20brainstorming")] "Here"
+
+tickerizeApi :: Web sig m => ServerT TickerizeApi m
+tickerizeApi = tickerize 
+  where
+    tickerize :: Web sig m => Text -> m Text
+    tickerize input = do
+          AppState{tickerTrie = t} <- ask @AppState
+          return . T.unwords $ processInput t <$> T.words input
+
+    processInput :: Trie () -> Text -> Text
+    processInput t input = 
+      let firstSymbol = doLookup input
+          (_, firstRemainder) = T.splitAt (T.length firstSymbol) input
+          (secondSymbol, secondRemainder) = handleChunk firstRemainder
+          (thirdSymbol, leftover) = handleChunk secondRemainder
+      in firstSymbol <> secondSymbol <> thirdSymbol <> leftover
+      where
+        doLookup x = case TE.match t . TTE.encodeUtf8 $ T.toUpper x of
+            Nothing -> x
+            Just (x0, _, _) -> TTE.decodeUtf8 x0
+        
+        handleChunk x = case T.length x of
+            0 -> (x, "")
+            1 -> (x, "")
+            _ -> let 
+              firstLetter = T.take 1 x
+              remaining = T.drop 1 x
+              looked = doLookup remaining
+              (_, leftover) = T.splitAt (T.length looked) remaining
+              in (firstLetter <> looked, leftover)
+
+apiProxy :: Proxy Api
+apiProxy = Proxy
+
+server :: ServerT Api WebConcrete
+server = healthApi :<|> tickerizeApi :<|> rootEndpoint
+  where
+    healthApi :: Web sig m => ServerT HealthApi m
+    healthApi = return "I'm alive!"
+
 appToHandler :: AppState -> WebConcrete a -> Handler a
 appToHandler appEnv =
   Handler
@@ -68,65 +100,10 @@ appToHandler appEnv =
     . runM
     . runReader @AppState appEnv
 
-type HealthApi = "health" :> "alive" :> Get '[JSON] Text
-
-type TickerizeApi = "tickerize" :> Capture "input" Text :> Get '[JSON] Text
-
-type RootEndpoint = Get '[HTML] (Html ())
-
-rootEndpoint :: Web sig m => ServerT RootEndpoint m
-rootEndpoint = asdf
-  where
-    asdf :: Web sig m => m (Html ())
-    asdf = do
-      AppState{baseUrl = t} <- ask @AppState
-      return $ body_ $ p_ "To use this site, navigate to " <> with a_ [href_  (coerce t <> "/tickerize/allegedly%20brainstorming")] "Here"
-
-healthApi :: Web sig m => ServerT HealthApi m
-healthApi = return "Ok"
-
-tickerizeApi :: Web sig m => ServerT TickerizeApi m
-tickerizeApi = tickerize 
-
-tickerize :: Web sig m => Text -> m Text
-tickerize input = do
-      AppState{tickerTrie = t} <- ask @AppState
-      return . T.unwords $ processInput t <$> T.words input
-
-processInput :: Trie () -> Text -> Text
-processInput t input = 
-  let firstSymbol = doLookup input
-      (_, firstRemainder) = T.splitAt (T.length firstSymbol) input
-      (secondSymbol, secondRemainder) = handleChunk firstRemainder
-      (thirdSymbol, leftover) = handleChunk secondRemainder
-  in firstSymbol <> secondSymbol <> thirdSymbol <> leftover
-  where
-    doLookup x = case TE.match t . TTE.encodeUtf8 $ T.toUpper x of
-        Nothing -> x
-        Just (x0, _, _) -> TTE.decodeUtf8 x0
-    
-    handleChunk x = case T.length x of
-        0 -> (x, "")
-        1 -> (x, "")
-        _ -> let 
-          firstLetter = T.take 1 x
-          remaining = T.drop 1 x
-          looked = doLookup remaining
-          (_, leftover) = T.splitAt (T.length looked) remaining
-          in (firstLetter <> looked, leftover)
-
-type Api = HealthApi :<|> TickerizeApi :<|> RootEndpoint
-
-apiProxy :: Proxy Api
-apiProxy = Proxy
-
-server :: ServerT Api WebConcrete
-server = healthApi :<|> tickerizeApi :<|> rootEndpoint
-
 runServer :: AppState -> IO ()
-runServer appState@AppState {port = Port port} = do
+runServer appState@AppState {port = port} = do
   print (("Running server on port " <> show port) :: Text)
-  run port app
+  run (openPort port) app
   where
     handler :: WebConcrete a -> Handler a
     handler = appToHandler appState
