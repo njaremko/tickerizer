042c18570a1feecc341d0b6c5b5e6573a73ee13c
Nathan Jaremko <njaremko@wealthsimple.com>
2022-03-16 19:01:35 -0400

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,8 @@
-use std::collections::HashMap;
-
 use radix_trie::Trie;
-use radix_trie::TrieCommon;
 use serde::Serialize;
 use serde_json::Value;
 use urlencoding::decode;
+use utils::{process_words, parse_params, build_trie};
 use worker::*;
 
 mod tickers;
@@ -22,62 +20,10 @@ fn log_request(req: &Request) {
     );
 }
 
-fn process_word(t: &Trie<String, String>, s: &str) -> String {
-    let mut result = "".to_string();
-
-    let first_match = t.get_ancestor(&s.to_uppercase());
-    let first_ticker = first_match
-        .map(|q| q.key().map(|t| t.as_ref()).unwrap_or(""))
-        .unwrap_or_else(|| s);
-
-    result += first_ticker;
-    let remaining = &s[first_ticker.len()..];
-
-    if first_ticker.len() + 3 < s.len() {
-        let second_match = t.get_ancestor(&remaining[2..].to_uppercase());
-        let second_ticker = second_match
-            .map(|q| q.key().map(|t| t.as_ref()).unwrap_or(""))
-            .unwrap_or_else(|| s);
-        result += &remaining[0..2];
-        result += second_ticker;
-        result += &remaining[second_ticker.len()+2..];
-    } else {
-        result += &remaining;
-    }
-
-    result
-}
-
-fn process_words(trie: &Trie<String, String>, s: &str) -> String {
-    s.split_whitespace()
-        .map(|word| process_word(trie, word))
-        .collect::<Vec<String>>()
-        .join(" ")
-}
-
 #[derive(Serialize)]
 struct SlackResponse {
     text: String,
     response_type: String,
-}
-
-fn parse_params(params: &str) -> HashMap<String, String> {
-    params
-        .split('&')
-        .map(|kv| {
-            let pair = kv
-                .split('=')
-                .take(2)
-                .map(|t| t.to_string())
-                .collect::<Vec<String>>();
-            let key = pair.get(0).unwrap();
-            let val = pair.get(1).unwrap();
-            (key.to_owned(), val.to_owned())
-        })
-        .fold(HashMap::new(), |mut accum, (k, v)| {
-            accum.insert(k, v);
-            accum
-        })
 }
 
 #[event(fetch)]
@@ -99,11 +45,7 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
         .get("/", |_, _| Response::ok("Hello from Workers!"))
         .get_async("/tickerize/:field", |_req, ctx| async move {
             if let Some(name) = ctx.param("field") {
-                let mut trie = Trie::new();
-                TICKERS.iter().for_each(|f| {
-                    trie.insert(f.to_string(), f.to_string());
-                });
-
+                let trie = build_trie();
                 let r = decode(name).map_err(|_e| "")?.to_string();
                 let resp = process_words(&trie, &r);
                 Response::ok(resp)
@@ -112,13 +54,7 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
             }
         })
         .post_async("/slack/tickerize", |mut req, _ctx| async move {
-            let mut trie = Trie::new();
-            TICKERS.iter().for_each(|f| {
-                trie.insert(f.to_string(), f.to_string());
-            });
-
             let form_data = req.form_data().await?;
-
             let text_param = match form_data.get("text") {
                 Some(x) => match x {
                     FormEntry::Field(x) => x,
@@ -126,9 +62,8 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
                 },
                 None => "".into(),
             };
-
+            let trie = build_trie();
             let text = process_words(&trie, &text_param);
-
             Response::from_json(&SlackResponse {
                 response_type: "in_channel".into(),
                 text,
@@ -145,18 +80,15 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
         .get_async("/slack/oauth", |req, ctx| async move {
             let client_id = ctx.var("SLACK_CLIENT_ID")?;
             let client_secret = ctx.secret("SLACK_CLIENT_SECRET")?;
-            let query = req
+            let params = req
                 .url()?
                 .query()
-                .map(|c| c.to_owned())
-                .unwrap_or_else(|| "".into());
-            console_log!("This is the query we got: {query}");
-            let params = parse_params(&query);
+                .map(|c| parse_params(c))
+                .unwrap_or_default();
             let code = params
                 .get("code")
-                .map(|c| c.to_owned())
-                .unwrap_or_else(|| "".into());
-            console_log!("This is the code we got: {code}");
+                .map(|c| c.as_ref())
+                .unwrap_or("");
             let uri = "https://slack.com/api/oauth.v2.access?client_id=".to_string()
                 + &client_id.to_string()
                 + "&client_secret="
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,9 @@
+use std::collections::HashMap;
+
 use cfg_if::cfg_if;
+use radix_trie::{Trie, TrieCommon};
+
+use crate::tickers::TICKERS;
 
 cfg_if! {
     // https://github.com/rustwasm/console_error_panic_hook#readme
@@ -9,4 +14,64 @@ cfg_if! {
         #[inline]
         pub fn set_panic_hook() {}
     }
+}
+
+pub fn build_trie() -> Trie<String, String> {
+    let mut trie = Trie::new();
+    TICKERS.iter().for_each(|f| {
+        trie.insert(f.to_string(), f.to_string());
+    });
+    trie
+}
+
+fn process_word(t: &Trie<String, String>, s: &str) -> String {
+    let mut result = "".to_string();
+
+    let first_match = t.get_ancestor(&s.to_uppercase());
+    let first_ticker = first_match
+        .map(|q| q.key().map(|t| t.as_ref()).unwrap_or(""))
+        .unwrap_or_else(|| s);
+
+    result += first_ticker;
+    let remaining = &s[first_ticker.len()..];
+
+    if first_ticker.len() + 3 < s.len() {
+        let second_match = t.get_ancestor(&remaining[2..].to_uppercase());
+        let second_ticker = second_match
+            .map(|q| q.key().map(|t| t.as_ref()).unwrap_or(""))
+            .unwrap_or_else(|| s);
+        result += &remaining[0..2];
+        result += second_ticker;
+        result += &remaining[second_ticker.len() + 2..];
+    } else {
+        result += &remaining;
+    }
+
+    result
+}
+
+pub fn process_words(trie: &Trie<String, String>, s: &str) -> String {
+    s.split_whitespace()
+        .map(|word| process_word(trie, word))
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
+pub fn parse_params(params: &str) -> HashMap<String, String> {
+    params
+        .split('&')
+        .map(|kv| {
+            let pair = kv
+                .split('=')
+                .take(2)
+                .map(|t| t.to_string())
+                .collect::<Vec<String>>();
+            let key = pair.get(0).unwrap();
+            let val = pair.get(1).unwrap();
+            (key.to_owned(), val.to_owned())
+        })
+        .fold(HashMap::new(), |mut accum, (k, v)| {
+            accum.insert(k, v);
+            accum
+        })
 }
